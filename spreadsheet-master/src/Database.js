c2dc1545c68b5b6fee1a0216c139245344713173
Jonatan Lagland <e1700814@edu.vamk.fi>
2021-11-09 13:03:47 +0200

    Basic fetch

--- spreadsheet-master/src/Database.js
+++ spreadsheet-master/src/Database.js
@@ -1,4 +1,6 @@
 import React, { useState, useEffect } from 'react';
+import Sheet from "./Sheet";
+
 
 const DataBase = () => {
     const [data, setData] = useState([]);
@@ -7,16 +9,16 @@ const DataBase = () => {
     }, []);
 
     function fetchData() {
-        fetch("http://projectware.net:8890/sparql?default-graph-uri=urn%3Asparql%3Abind%3Avamk-data&query=select+*+%7B%3Fs+%3Fp+%3Fo%7D&should-sponge=&format=application%2Fsparql-results%2Bjson&timeout=0&debug=on&run=+Run+Query+")
+        fetch("http://projectware.net:8890/sparql?default-graph-uri=urn%3Asparql%3Abind%3Avamk-data&query=SELECT+%3Fo+WHERE+%7B+%3Fs+%3Fp+%3Fo+%7D&should-sponge=&format=application%2Fsparql-results%2Bjson&timeout=0&debug=on&run=+Run+Query+")
             .then(response => response.json())
             .then(jsonData => {
                 console.log(jsonData);
-                jsonData && jsonData.results.bindings && setData(jsonData.results.bindings);
+                jsonData && jsonData.results.bindings && setData(jsonData.results.bindings[3].o.value);
             })
     }
 
     return (
-        <div><h1>Sparcomponent</h1>
+        <div>
             <div>{JSON.stringify(data)}</div>
         </div>
     )
